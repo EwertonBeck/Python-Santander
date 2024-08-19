c947e70a2e91c4fab425b18efcab05d943ab558a
Ewerton Beck <ewertonbeck@hotmail.com>
2024-08-18 23:29:36 -0300

    inicio do curso

---
+++ Ola_mundo.py
@@ -0,0 +1,13 @@
+print("Olá Mundo!")
+#Este é um comentário de uma linha
+
+# Escrevendo varios comandos em uma unica linha 
+print("olá"); print("Mundo!")
+
+#uso do parentese para agrupar expressões
+a=2
+b=4
+c=6
+print(a+b+c)
+
+#módulo 2.1 Variáveis
