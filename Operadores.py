1c0dca69b7b2caf4b4d4c2b617934ec4c6eb72a1
Ewerton Beck <ewertonbeck@hotmail.com>
2024-08-19 23:26:18 -0300

    operadoes em python

---
+++ Operadores.py
@@ -0,0 +1,19 @@
+# Operadores Aritiméticos
+
+a=10
+b=3
+soma= a+b
+subtrai= a-b
+multiplica= a*b
+divisao= a/b
+divide_int= a//b
+modulo= a%b
+potencia= a**b
+
+print(soma)
+print(subtrai)
+print(multiplica)
+print(divisao)
+print(divide_int)
+print(modulo)
+print(potencia)
